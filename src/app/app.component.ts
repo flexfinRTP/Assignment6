543f1d043e02ba20c8642f41f9342adcec5c2bd8
flexfinRTP <gaffney311@gmail.com>
2020-12-15 01:12:45 -0600

    v.02 step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+//REDO FETCHEDSATELLITES STUFF
 export class AppComponent {
   title = 'orbit-report';
-}
+  sourceList: Satellite[];
+  displayList: Satellite[] = [];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for(let i=0; i < fetchedSatellites.length; i++){
+            this.sourceList.push(new Satellite(fetchedSatellites[i].name, 
+              fetchedSatellites[i].type, 
+              fetchedSatellites[i].launchDate, 
+              fetchedSatellites[i].orbitType, 
+              fetchedSatellites[i].operational))
+            this.displayList = this.sourceList.slice(0);
+          }
+
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+ }
+
+
+};
